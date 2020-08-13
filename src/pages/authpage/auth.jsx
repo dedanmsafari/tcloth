09c391fe823d748c75106a287a36a8fe0fb9f496
Dedan Msafari <dedan.msafari@gmail.com>
2020-08-13 21:50:52 +0300

    auth component and its scss file

---
+++ src/pages/authpage/auth.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SignIn from "../../components/signIn/signIn";
+import "./auth.scss";
+
+const Auth = () => {
+  return <div className="sign-in-and-sign-up">
+  
+  <SignIn/>
+  </div>;
+};
+
+export default Auth;
