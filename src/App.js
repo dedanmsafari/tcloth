db00a008438ed19ef02ec84598077e16a370295e
Dedan Msafari <dedan.msafari@gmail.com>
2020-08-16 20:47:31 +0300

    app component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import React from "react";
-import HomePage from "./homepage";
+import { Switch, Route } from "react-router-dom";
+import HomePage from "./pages/homepage/homepage";
+import ShopPage from "./pages/shop/shoppage";
+import Header from "./components/header/header";
+import Auth from "./pages/authpage/auth";
+import { auth,createUserProfileDocument } from "./firebase/firebase.utils";
 import "./App.css";
 
-function App() {
-  return (
-    <div>
-      <HomePage />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (user) => {
+     createUserProfileDocument(user);
+    });
+  }
+
+  componentWillUnmount(){
+    this.unsubscribeFromAuth();
+  }
+
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/shop" component={ShopPage} />
+          <Route exact path="/signin" component={Auth} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
