e0612bf88bb239ab7b006b96b8eb8411eae49a6a
Dedan Msafari <dedan.msafari@gmail.com>
2020-08-17 11:58:44 +0300

    minor changes to auth component

--- src/pages/authpage/auth.jsx
+++ src/pages/authpage/auth.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import SignIn from "../../components/signIn/signIn";
+import SignUp from "../../components/signup/signUp";
 import "./auth.scss";
 
 const Auth = () => {
-  return <div className="sign-in-and-sign-up">
-  
-  <SignIn/>
-  </div>;
+  return (
+    <div className="sign-in-and-sign-up">
+      <SignIn />
+      <SignUp />
+    </div>
+  );
 };
 
 export default Auth;
