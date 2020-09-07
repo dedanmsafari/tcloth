331eb8d6d92c78c5864e94374e6a62e20217e8fd
Dedan Msafari <dedan.msafari@gmail.com>
2020-09-07 05:15:15 +0300

    modified root reducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,6 +1,25 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 
 import userReducer from "./user/userReducer";
 import cartReducer from "./cart/cartReducer";
+import directoryReducer from "./directory/directoryReducer";
+import shopReducer from "./shop/shopReducer";
 
-export default combineReducers({ user: userReducer, cart: cartReducer });
+const persistConfig = {
+  key: "root",
+  storage,
+  stateReconciler: autoMergeLevel2,
+  whitelist: ["cart"],
+};
+const rootReducer = combineReducers({
+  user: userReducer,
+  cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer
+});
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export default persistedReducer;
