65958b7f935dcc0d96faff560a802264b6871ddf
Dedan Msafari <dedan.msafari@gmail.com>
2020-09-07 05:17:05 +0300

    store.js file

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,13 @@
 import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import logger from "redux-logger";
-
-import rootReducer from "./rootReducer";
+import { persistStore } from "redux-persist";
+import persistedReducer from "./rootReducer";
 
 const middlewares = [logger];
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
-
-export default store;
+const store = createStore(
+  persistedReducer,
+  composeWithDevTools(applyMiddleware(...middlewares))
+);
+const persistor = persistStore(store);
+export { store, persistor };
