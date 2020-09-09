bd2e8715ce24b24dd3375cc6d3962aecde9112d9
Dedan Msafari <dedan.msafari@gmail.com>
2020-09-09 22:21:20 +0300

    shop selelctor

--- src/redux/shop/shopSelectors.js
+++ src/redux/shop/shopSelectors.js
@@ -1,7 +1,26 @@
 import { createSelector } from "reselect";
+import memoize from 'lodash.memoize';
+const COLLECTION_ID_MAP = {
+  hats: 1,
+  sneakers: 2,
+  jackets: 3,
+  womens: 4,
+  mens: 5,
+};
+
 const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector(
   [selectShop],
-  (shop) => shop.collection
+  (shop) => shop.collections
 );
+
+export const selectCollection = memoize((collectionUrlParam) =>
+  createSelector(
+    [selectCollection],
+    
+    (collections) =>
+    collections.find(
+      (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+    )
+  ));
