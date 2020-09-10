1799db85de2e8651c5ec2ad53398907a43cf876f
Dedan Msafari <dedan.msafari@gmail.com>
2020-09-10 21:02:13 +0300

    CHANGED ENV VARIABLES

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,12 @@ import logger from "redux-logger";
 import { persistStore } from "redux-persist";
 import persistedReducer from "./rootReducer";
 
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger)
+}
+
 const store = createStore(
   persistedReducer,
   composeWithDevTools(applyMiddleware(...middlewares))
