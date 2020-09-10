a72e9b48ca0f94b9972baf5aaf658243a3072595
Dedan Msafari <dedan.msafari@gmail.com>
2020-09-10 16:22:25 +0300

    minor modifications

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/checkout" component={CheckOut} />
-          <Route exact path="/shop" component={ShopPage} />
+          <Route  path="/shop" component={ShopPage} />
           <Route
             exact
             path="/signin"
