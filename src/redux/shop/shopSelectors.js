89cd9aee3a57f51f1f0652cac36c2fe3eeff9254
Dedan Msafari <dedan.msafari@gmail.com>
2020-09-25 21:54:27 +0300

    modified selector

--- src/redux/shop/shopSelectors.js
+++ src/redux/shop/shopSelectors.js
@@ -1,8 +1,7 @@
 import { createSelector } from "reselect";
-import memoize from 'lodash.memoize';
+import memoize from "lodash.memoize";
 
 const selectShop = (state) => state.shop;
-
 
 export const selectCollections = createSelector(
   [selectShop],
@@ -11,12 +10,23 @@ export const selectCollections = createSelector(
 
 export const selectCollectionsForPreview = createSelector(
   [selectCollections],
-  collections => Object.keys(collections).map(key => collections[key])
-)
-
+  //review later convert collections object to array?
+  (collections) =>
+    collections ? Object.keys(collections).map((key) => collections[key]) : []
+);
 
 export const selectCollection = memoize((collectionUrlParam) =>
-  createSelector(
-    [selectCollections],
-  collections => collections[collectionUrlParam]
-  ));
+  createSelector([selectCollections], (collections) =>
+    collections ? collections[collectionUrlParam] : null
+  )
+);
+
+export const selectIsCollectionFetching = createSelector(
+  [selectShop],
+  shop => shop.isFetching
+);
+
+export const selectIsCollectionsLoaded = createSelector(
+  [selectShop],
+  shop => !!shop.collections
+)
