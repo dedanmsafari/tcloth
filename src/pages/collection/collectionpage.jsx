bb7da3a7a2d8f5e7ff102bb898fb53d894c27c65
Dedan Msafari <dedan.msafari@gmail.com>
2020-09-25 21:54:53 +0300

    collection page

--- src/pages/collection/collectionpage.jsx
+++ src/pages/collection/collectionpage.jsx
@@ -6,7 +6,7 @@ import CollectionItem from "../../components/collectionItem/collectionItem";
 
 const CollectionPage = ({ collection }) => {
   const { title, items } = collection;
-  
+ 
   return (
     <div className="collection-page">
       <h2 className="title">{title}</h2>
