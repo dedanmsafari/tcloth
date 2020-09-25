f05b69c687cc81d20072c9bccc97040baa6a2f69
Dedan Msafari <dedan.msafari@gmail.com>
2020-09-25 21:54:14 +0300

    thunk reducers

--- src/redux/shop/shopReducer.js
+++ src/redux/shop/shopReducer.js
@@ -1,11 +1,30 @@
-import SHOP_DATA from "./shopdata";
+import ShopActionTypes from "./shopTypes";
 
 const INITIAL_STATE = {
-  collections: SHOP_DATA,
+  collections: null,
+  isFetching: false,
+  errorMessage: undefined,
 };
 
 const shopReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case ShopActionTypes.FETCH_COLLECTIONS_START:
+      return {
+        ...state,
+        isFetching: true,
+      };
+    case ShopActionTypes.FETCH_COLLECTIONS_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        errorMessage: action.payload,
+      };
+    case ShopActionTypes.FETCH_COLLECTIONS_SUCCESS:
+      return {
+        ...state,
+        collections: action.payload,
+        isFetching: false,
+      };
     default:
       return state;
   }
