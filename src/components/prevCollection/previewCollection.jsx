fb7b602bdef47636d30f782844a351894bb35de8
dedanmsafari <dedan.msafari@gmail.com>
2021-04-15 19:16:37 +0300

    fixed problems with preview collection

--- src/components/prevCollection/previewCollection.jsx
+++ src/components/prevCollection/previewCollection.jsx
@@ -1,14 +1,21 @@
-import React from "react";
-import CollectionItem from "../collectionItem/collectionItem";
-import "./prevCollection.scss";
+import React from 'react';
+import CollectionItem from '../collectionItem/collectionItem';
+import './prevCollection.scss';
+import { OptionLink } from '../../components/header/headerstyles';
 const PrevCollection = ({ title, items }) => {
   return (
-    <div className="collection-preview">
-      <h1 className="title">{title.toUpperCase()}</h1>
-      <div className="preview">
+    <div className='collection-preview'>
+      <h1 className='title'>
+        {' '}
+        <OptionLink to={`/shop/${title.toLowerCase()}`}>
+          {title.toUpperCase()}
+        </OptionLink>
+      </h1>
+
+      <div className='preview'>
         {items
           .filter((item, idx) => idx < 4)
-          .map(item => (
+          .map((item) => (
             <CollectionItem key={item.id} item={item} />
           ))}
       </div>
